2823fea9c7ea6eaab47f493a6dcecc2c555f46d7
Muhammed Raihan P A <99407382+pu-raihan@users.noreply.github.com>
2023-08-10 03:44:30 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"]
+    origin: ["http://localhost:3000", "http://localhost:3001"], credentials:true
   })
 );
 
