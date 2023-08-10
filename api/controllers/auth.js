7dbab19cc95243689a31620100d5a0f4a2fe0ddb
Muhammed Raihan P A <99407382+pu-raihan@users.noreply.github.com>
2023-08-10 03:49:23 -0800

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -80,7 +80,7 @@ export const login = (req, res) => {
 
         res
           .cookie("accessToken", token, {
-            httpOnly: true,
+            httpOnly: false,
           })
           .status(200)
           .json(others);
