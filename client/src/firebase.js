a22c6ae90508920e924f74622f0845a5e662b61e
Muhammed Raihan P A <99407382+pu-raihan@users.noreply.github.com>
2023-08-10 07:18:23 -0800

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,11 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: process.env.GOOGLE_API,
-=======
-  apiKey: "AIzaSyCW5RqtSNQOV-HALonWw33kHcqYVnN_0rI",
->>>>>>> 891a828c89ea96e18ddc6a521501bb5aa02036aa
   authDomain: "cambuzz-b0650.firebaseapp.com",
   projectId: "cambuzz",
   storageBucket: "cambuzz.appspot.com",
