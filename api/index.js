b1281f8540594b2acc0f035be5c9590e10a4a755
Muhammed Raihan P A <99407382+pu-raihan@users.noreply.github.com>
2023-08-10 09:11:58 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ import multer from "multer";
 import cookieParser from "cookie-parser";
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', {'https://cambuzz.netlify.app',"http://localhost:3000"});
+  res.header('Access-Control-Allow-Origin', ['https://cambuzz.netlify.app',"http://localhost:3000"]);
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
