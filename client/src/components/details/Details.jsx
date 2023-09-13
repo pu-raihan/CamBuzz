d8b15693f3895d4fccba95c023a80b4421de63d2
pu-raihan <21352030@pondiuni.ac.in>
2023-09-13 23:09:45 +0530

    location access err handle

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -24,7 +24,7 @@ const Details = () => {
     const [mapOpen, setMapopen] = useState(false);
     const [err, setErr] = useState(null);
 
-    const [isLoc, setIsLoc] = useState(false);
+    const [isLoc, setIsLoc] = useState(true);
 
 
     const resource = useLocation().pathname.split("/")[2];
