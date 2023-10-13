8ef56077358115cbc466f3a370d4bad3c97195fa
Muhammed Raihan P A <99407382+pu-raihan@users.noreply.github.com>
2023-10-12 18:10:16 -0800

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://cambuzz-api.onrender.com/api",
+  // baseURL: "https://cambuzz-api.onrender.com/api",
+  baseURL: "https://cambuzz.cyclic.app/api",
   withCredentials: true,
 });
